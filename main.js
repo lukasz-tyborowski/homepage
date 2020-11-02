dfb393749375ecef1a2d09525afc13621cf54452
Łukasz Tyborowski <lu.tyborowski@gmail.com>
2020-11-02 21:16:12 +0100

    1st homework - styles

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 //alert('Siema, nazywam się Łukasz i witam Cię w moim pierdolniczku XD')
 //wyjebałem bo wkurwiało
 
-console.log('Siema, nazywam się Łukasz i witam Cię w moim pierdolniczku XD')
+console.log("Siema, nazywam się Łukasz i witam Cię w moim pierdolniczku XD");
 
-const firstName = 'Łukasz';
+const firstName = "Łukasz";
 const age = 28;
 
 console.log(firstName);
 console.log(age);
 console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
+
+const heading = document.querySelector("body");
+
+console.log(heading);
