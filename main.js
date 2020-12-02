b6ffc941e6f68caa87a7c0cee469354b62360b6f
Łukasz Tyborowski <lu.tyborowski@gmail.com>
2020-12-02 20:45:34 +0100

    jsy podstawy obiektów

--- main.js
+++ main.js
@@ -13,3 +13,57 @@ console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
 const heading = document.querySelector(".dom-test--js");
 
 console.log(heading);
+
+function calculate(myNumber) {
+    return myNumber*7;
+}
+
+
+const myResult = calculate(1);
+
+console.log(myResult);
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+createContent('.about__heading--js', 'siema siema');
+
+
+function helloWorld() {
+    console.log('witaj świecie')
+}
+helloWorld();
+
+/*const helloWorld = () => { //fat arrow function
+    console.log('witaj świecie')
+}
+helloWorld();*/
+
+
+
+
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed`)
+    },
+    isOperating: true,
+    name: 'Death Star',
+    levels: 357,
+    population: 10000,
+    isLightOn: true,
+    commander: {
+        name: 'Darth Vader',
+        age: 44,
+    }
+}
+
+console.log (deathStar)
+
+deathStar.fire('brain')
+
+console.log(deathStar.name) //wypisuje tylko nazwę obiektu
+
+console.log(deathStar['name']) //nazwe własności podajemy jako string
