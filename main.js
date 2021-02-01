e2ee1a468b173e03437b58586a5a07c4253521ee
Marcin Tyborowski <marcin.tyborowski@billennium.com>
2021-02-01 20:21:00 +0100

    add left to header, fix typo in main.js, add fa icons to project

--- main.js
+++ main.js
@@ -133,5 +133,5 @@ const hamburger = document.querySelector('.hamburger--js');
 
 hamburger.addEventListener('click', () => {
   const nav = document.querySelector('.navigation--js');
-  nav.classList.toggle('navigation--open)');
+  nav.classList.toggle('navigation--open');
 })
